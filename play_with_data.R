04823badc5a1600c6f1e4a3207c7a50b3251c797
alexaoh <alexaoh@users.noreply.github.com>
2022-11-09 11:03:22 +0100

    Added more code and some data from Markov.
    Running the whole thing again with 25 states now, will upload the data
    when ready.

--- play_with_data.R
+++ play_with_data.R
@@ -1,5 +1,5 @@
 # Play with the data (start working on the project)
-
+set.seed(1234)
 library(R.matlab)
 library(ggplot2)
 library(dplyr)
@@ -31,10 +31,9 @@ ang_test <- angdata[(train_indices+1):N]
 
 # Fitting a HMM can e.g. be done with https://cran.r-project.org/web/packages/depmixS4/index.html
 library(depmixS4)
-?depmix
 
 # Hyperparameters and priors.
-nstates <- 10 # Fixed number of states. 
+nstates <- 25 # Fixed number of states. 
 pi <- rep(1, nstates)/nstates # Uniform prior state probabilities. 
 
 # Try to fit a HMM. 
@@ -45,7 +44,8 @@ cell_train_df <- as.data.frame(cell_train)
 # Quick way to make the list of formulas for the multivariate HMM. 
 formel <- list()
 families <- list()
-for (i in 1:10){ # Only tried with 10 of the neurons for now, because of long fitting times. 
+for (i in 1:71){ # Only tried with 10 of the neurons for now, because of long fitting times. 
+  # Running with all 71 neurons on Markov in order to see what happens. 
   formel[[i]] <- as.formula(paste0(paste0("V",i), "~1"))
   families[[i]] <- poisson(link = "log")
 }
@@ -54,6 +54,9 @@ model <- depmix(formel, family = families, data = cell_train_df,
                 nstates = nstates, instart = pi) #  try to use pi instead for the initial state probabilities here. 
 fm <- fit(model)
 summary(fm)
+
+#save(fm, file = "fitted_HMM.RData") # Save the fitted model such that we can load it locally.
+load("fitted_HMM.RData", verbose = T)
 
 # Now, what can we do with the states?
 # How has Ben plotted them in the lecture on project description?
@@ -64,6 +67,12 @@ table(inferred_states)
 # Play with the transition matrix. 
 transition_matrix <- matrix(getpars(fm)[(nstates+1):(nstates^2+nstates)],
                             byrow=TRUE,nrow=nstates,ncol=nstates)
+
+#save(transition_matrix, file = "transition_matrix_full.RData") # Save the transition matrix such that we can load it locally.
+load("transition_matrix_full.RData", verbose = T)
+
+# Plot the transition matrix. Try to rearrange it such that we can c
+transition_matrix %>% heatmap(Colv = NA)
 
 # Try to do PCA on transition matrix. 
 pca <- princomp(transition_matrix)
@@ -81,6 +90,26 @@ plt <- tibble(df) %>%
   theme_minimal()
 print(plt) # Not at all a circle :))
 
+# Try with a npn-linear dim. red. method, since PCA does not capture a lot of the variability in the first components. 
+library(Rtsne)
+tsne_out <- Rtsne(transition_matrix, pca = F, perplexity = as.integer(nstates/3)-1, theta = 0.8)
+data.frame(cbind(tsne_out$Y,"state"=1:15)) %>% 
+  ggplot(aes(x = V1, y = V2)) +
+  geom_point() +
+  geom_text(aes(label = state), nudge_x = 1.5) +
+  ggtitle(paste0("t-SNE on transition matrix")) +
+  theme_minimal() # Not really a circle tbh. 
+
 
 # Perhaps try to group the angular data in each state together, to see if each state "takes up" one specific direction.
 # Then we could plot the recorded angles and see if they "coincide" with the states. 
+hcl <- hclust(dist(transition_matrix), method = "ward.D")
+cut <- cutree(hcl, k = 4) # We cut it into 4 groups. 
+plot(hcl) # Produce the same dendrogram as in the heatmap above. 
+rect.hclust(hcl, k = 4, border = 2:6)
+
+# We group the inferred states according to the cut above. 
+grouped_inferred_states <- cut[inferred_states]
+
+# Group the angle data according to the states and then eventually by the cuts.
+# Not sure how to do this right now?! HELP
